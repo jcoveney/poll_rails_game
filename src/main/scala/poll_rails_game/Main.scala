32698ed42c3f617e407c129e434b72632aad6846
Jonathan Coveney <jcoveney@gmail.com>
2020-04-15 19:40:06 +0800

    Get rid of (now invalid) dropbox key

--- src/main/scala/poll_rails_game/Main.scala
+++ src/main/scala/poll_rails_game/Main.scala
@@ -2,6 +2,13 @@ package poll_rails_game
 
 object Main {
   def main(args: Array[String]): Unit = {
-    Dropbox.checkInfo()
+    val ACCESS_TOKEN = System.getenv("RAILS_POLL_ACCESS_TOKEN")
+    //TODO proper logging
+    //TODO use proper argument handling and pass as an argument?
+    if (ACCESS_TOKEN == null)
+      throw new IllegalArgumentException("need to set RAILS_POLL_ACCESS_TOKEN environment variable")
+
+
+    Dropbox.checkInfo(ACCESS_TOKEN)
   }
 }
